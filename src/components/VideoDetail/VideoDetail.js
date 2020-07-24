69748f2fdd06048d33ff9d3acbabca3a48221b9a
tatsuro <oscarwild13@gmail.com>
2020-07-24 19:22:26 +0900

    APIを同時に取得するためPrimise.allにリファクタリングと動画詳細ページのスタイルをSP調整

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
-import { fetchSelectedData } from '../../apis';
+import React, { useContext } from 'react';
 import { Store } from '../../store';
 import VideoPlay from '../VideoPlay/VideoPlay';
 import Style from './VideoDetail.module.scss';
 import Linkify from 'react-linkify';
 
 const VideoDetail = () => {
-  const { globalState, setGlobalState } = useContext(Store);
-  const location = useLocation();
-  const setSelectedVideo = async () => {
-    const searchParams = new URLSearchParams(location.search);
-    const id = searchParams.get('v');
-    //console.log(id)
-    await fetchSelectedData(id).then((res) => {
-      const item = res.data.items.shift();
-      setGlobalState({ type: 'SET_SELECTED', payload: { selected: item } });
-      //console.log("res", res)
-    });
-  };
-  useEffect(() => {
-    setSelectedVideo();
-    // eslint-disable-next-line
-  }, [location.search]);
+  const { globalState } = useContext(Store);
   return globalState.selected && globalState.selected.id ? (
     <div className={Style.wrap}>
       <VideoPlay id={globalState.selected.id} />
--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Layout from '../components/Layout/Layout';
 import VideoDetail from '../components/VideoDetail/VideoDetail';
 import SideList from '../components/SideList/SideList';
+import { Store } from '../store/index';
+import { useLocation } from 'react-router-dom';
+import { fetchSelectedData, fetchRelatedData } from '../apis';
 
 const Watch = () => {
+  // eslint-disable-next-line
+  const { setGlobalState } = useContext(Store);
+  const location = useLocation();
+  const setVideos = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get('v');
+    if (id) {
+      const [selected, related] = await Promise.all([
+        fetchSelectedData(id),
+        fetchRelatedData(id),
+      ]);
+      // console.log('result', result);
+      setGlobalState({
+        type: 'SET_SELECTED',
+        payload: { selected: selected.data.items.shift() },
+      });
+      setGlobalState({
+        type: 'SET_RELATED',
+        payload: { related: related.data.items },
+      });
+    }
+  };
+  useEffect(() => {
+    setVideos();
+    // eslint-disable-next-line
+  }, [location.search]);
   return (
     <Layout>
       <VideoDetail />
